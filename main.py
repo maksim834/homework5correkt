1cfad1e2d689da65a0fe1a832e787f4be6e0180f
maksim834 <mmankov924@gmail.com>
2024-07-06 22:33:03 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# my_string = input('Меня зовут Максим')
+# print(my_string)
+# print(my_string, 15)
+my_string=input("Меня зовут Максим".upper())
+print(my_string)
+my_string=input("Меня зовут Максим".lower())
+print(my_string)
+my_string=input("Меня зовут Максим".replace(' ',''))
+print(my_string)
+my_string=input("Меня зовут Максим".replace("Меня зовут Максим","М"))
+print(my_string)
+my_string=input("Меня зовут Максим".replace("Меня зовут Максим","м"))
+print(my_string)
